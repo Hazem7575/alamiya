dcb3ae199b068cf3e521550004b782b7d911a479
Mohamed Yasser <daanyp300@gmail.com>
2025-08-31 13:00:38 +0300

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -95,7 +95,7 @@ export interface EventsResponse {
 
 
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://alamiya.konhub.dev/api/api';
 
 class ApiClient {
   private getAuthHeaders(): Record<string, string> {
